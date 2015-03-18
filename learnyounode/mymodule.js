28d5c1fd8fb7afe6acd6b72b3db6a927b04ea9cf
Armour <497052684@qq.com>
2015-03-18 23:24:17 +0800

    change for to foreach, more elegant

--- learnyounode/mymodule.js
+++ learnyounode/mymodule.js
@@ -2,17 +2,14 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = function (dir, filter, callback) {		
-	var ext = '.' + filter;
 	fs.readdir(dir, function (err, list) {
 		if (err) 
-			callback(err, null);
+			return callback(err);	
 		else {
-			var answer = [];
-			for (var i = 0; i < list.length; i++) {
-				if (path.extname(list[i]) === ext)
-					answer.push(list[i]);
-			}
-			callback(null, answer);
+			list = list.filter(function (file) {
+				return path.extname(file) === '.' + filter;			
+			})
+			callback(null, list);
 		}	
 	});
 }
--- learnyounode/original.js
+++ learnyounode/original.js
@@ -3,6 +3,9 @@ var dir = process.argv[2];
 var filter = process.argv[3];
 
 mymodule(dir, filter, function (err, files) {
-	for (var i = 0; i < files.length; i++)
-		console.log(files[i]);
+	if (err) 
+		return console.error('Error: ', err);
+	files.forEach(function(file) {
+		console.log(file);
+	});
 });
