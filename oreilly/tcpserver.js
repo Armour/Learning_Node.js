89838a55a57a6a6e17edfc6c3c22f810d03470c4
Armour <497052684@qq.com>
2015-04-08 00:58:15 +0800

    make the tcpserver more robust, add log

--- oreilly/tcpserver.js
+++ oreilly/tcpserver.js
@@ -1,19 +1,41 @@
 var net = require("net");
 
 var chatServer = net.createServer();
-clientList = [];
+var clientList = [];
 
 chatServer.on("connection", function(client) {
 	client.name = client.remoteAddress + ":" + client.remotePort;
 	client.write("hi~ " + client.name);
+
 	clientList.push(client);
+
 	client.on("data", function(data) {
 		broadcast(data.toString(), client);
+	})
+
+	client.on("end", function(data) {
+		console.log(client.name + " quit");
+		clientList.splice(clientList.indexOf(client), 1);
+	})
+
+	client.on("error", function(e) {
+		console.log(e);
 	})
 }).listen(9000);
 
 function broadcast(message, client) {
-	for (var i = 0; i < clientList.length; i++)
-		if (clientList[i] !== client)
+	var cleanup = [];
+
+	for (var i = 0; i < clientList.length; i++) {
+		if (clientList[i] !== client) {
 			clientList[i].write(client.name + " says: " + message);
+		} else {
+			cleanup.push(clientList[i]);
+			clientList[i].destory();
+		}
+	}
+
+	for (var i = 0; i < cleanup.length; i++) {
+		clientList.splice(clientList.indexOf(cleanup[i]), 1);
+	}
 }
