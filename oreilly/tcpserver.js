f397ad9dc34d66e720db109ce483b93913117174
Armour <497052684@qq.com>
2015-04-08 00:42:13 +0800

    now it can spread messages in tcp form differnt client

--- oreilly/tcpserver.js
+++ oreilly/tcpserver.js
@@ -1,11 +1,19 @@
 var net = require("net");
 
 var chatServer = net.createServer();
+clientList = [];
 
 chatServer.on("connection", function(client) {
-	client.write("lalala\n");
-	//client.end();
-	client.on("data", function (data) {
-		console.log(data.toString());
+	client.name = client.remoteAddress + ":" + client.remotePort;
+	client.write("hi~ " + client.name);
+	clientList.push(client);
+	client.on("data", function(data) {
+		broadcast(data.toString(), client);
 	})
 }).listen(9000);
+
+function broadcast(message, client) {
+	for (var i = 0; i < clientList.length; i++)
+		if (clientList[i] !== client)
+			clientList[i].write(client.name + " says: " + message);
+}
